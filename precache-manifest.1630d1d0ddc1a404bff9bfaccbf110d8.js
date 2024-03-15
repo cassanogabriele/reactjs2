9f509e4c05f1ed2a5b3c07b067dda3e2e81e3ac3
cassano gabriele <gabriel_cassano@gmail.com>
2024-03-15 20:44:58 +0100

    transfert application reactjs

---
+++ precache-manifest.1630d1d0ddc1a404bff9bfaccbf110d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1da6f79ad80e02eb30a3",
+    "url": "/static/js/main.1da6f79a.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "1da6f79ad80e02eb30a3",
+    "url": "/static/css/main.66021726.chunk.css"
+  },
+  {
+    "revision": "858279bdbac404d9f90e049b379531e7",
+    "url": "/index.html"
+  }
+];
